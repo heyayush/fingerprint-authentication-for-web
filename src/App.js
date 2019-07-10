9876ebad1b97c35ed445fab53b42e36e1090e9b5
Ayush <ayush15may@gmail.com>
2019-07-10 13:50:15 +0530

    app install banner

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div id="app-install-banner">Wanna install this app?</div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -134,12 +134,13 @@ export function unregister() {
   }
 }
 
-// let deferredPrompt;
+let deferredPrompt;
 
-// window.addEventListener('beforeinstallprompt', (e) => {
-//   // Prevent Chrome 67 and earlier from automatically showing the prompt
-//   e.preventDefault();
-//   // Stash the event so it can be triggered later.
-//   deferredPrompt = e;
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
   
-// });
+  document.getElementById("app-install-banner").style.display = "block"
+});
