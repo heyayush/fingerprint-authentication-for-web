2037a6533058a45cbf9a66ee2d2f6cf533dc716a
Ayush <ayush15may@gmail.com>
2019-07-10 13:31:17 +0530

    pwa support

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -133,3 +133,13 @@ export function unregister() {
     });
   }
 }
+
+// let deferredPrompt;
+
+// window.addEventListener('beforeinstallprompt', (e) => {
+//   // Prevent Chrome 67 and earlier from automatically showing the prompt
+//   e.preventDefault();
+//   // Stash the event so it can be triggered later.
+//   deferredPrompt = e;
+  
+// });
