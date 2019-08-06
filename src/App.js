19dd053bd6fc160995c28cc89d66a2edc711672e
Ayush <ayush15may@gmail.com>
2019-08-06 14:31:37 +0530

    use cam

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
 import "./App.css";
 
-function App() {
+const App = () => {
+  const openCam = () => {
+    console.log("triggering camera");
+
+    const player = document.getElementById("player");
+
+    console.log("got the player node", player);
+
+    const constraints = {
+      video: true
+    };
+
+    navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+      player.srcObject = stream;
+    });
+  };
   return (
     <div className="App">
-      Hello World
+      <h2>Hello World</h2>
+      <button onClick={openCam}>Open Camera</button>
+      <div id="player" />
       <div id="app-install-banner">Click here to install this app</div>
     </div>
   );
-}
+};
 
 export default App;
