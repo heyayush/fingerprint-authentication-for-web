3b4611ec493147db2fd5d9e6fbb74b95aaffd121
Ayush <ayush15may@gmail.com>
2019-08-06 15:11:27 +0530

    Updates

---
+++ precache-manifest.293a72006c1d04074e6fbdc527b175d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fccd682bd0609b3042b0ee5528d09dc2",
+    "url": "/fingerprint-authentication-for-web/index.html"
+  },
+  {
+    "revision": "af2b4ff1954f91f56899",
+    "url": "/fingerprint-authentication-for-web/static/css/main.d8f9e272.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/fingerprint-authentication-for-web/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "af2b4ff1954f91f56899",
+    "url": "/fingerprint-authentication-for-web/static/js/main.5b9ebfad.chunk.js"
+  },
+  {
+    "revision": "e7135f50c41fc91b62a6",
+    "url": "/fingerprint-authentication-for-web/static/js/runtime~main.82eba045.js"
+  }
+]);
