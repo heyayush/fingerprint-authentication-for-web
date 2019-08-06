09d7316b06249e89aea7f8fb28fd12acd489228c
Ayush <ayush15may@gmail.com>
2019-08-06 17:24:48 +0530

    adding fingerprint icon

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
 import "./App.css";
-
+import fingerprintIcon from "./assets/icons/fingerprint.png";
 const App = () => {
   const openCam = () => {
     console.log("triggering camera");
 
-    const player = document.getElementById("player");
-
-    console.log("got the player node", player);
-
     const constraints = {
-      video: true
+      // audio: true
+      video: { facingMode: "user" }
     };
 
     navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+      const player = document.getElementById("player");
+      console.log("got the player node", player);
+
+      console.log("stream is", stream);
+
+      player.onloadedmetadata = function(e) {
+        player.play();
+      };
       player.srcObject = stream;
     });
   };
@@ -21,7 +26,14 @@ const App = () => {
     <div className="App">
       <h2>Hello World</h2>
       <button onClick={openCam}>Open Camera</button>
-      <div id="player" />
+      <video id="player" />
+      <div>Please authenticate using fingerprint</div>
+      <img
+        alt="fingerprint icon"
+        src={fingerprintIcon}
+        height="300px"
+        width="300px"
+      />
       <div id="app-install-banner">Click here to install this app</div>
     </div>
   );
